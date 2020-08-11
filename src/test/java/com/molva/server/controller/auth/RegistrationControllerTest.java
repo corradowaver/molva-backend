4ced4f88f7ca6a89c2303fde2abbd7acfa37304c
Georgii <49598870+corradowaver@users.noreply.github.com>
2020-08-11 18:18:11 +0300

    first impl

---
+++ src/test/java/com/molva/server/controller/auth/RegistrationControllerTest.java
@@ -0,0 +1,78 @@
+package com.molva.server.controller.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.molva.server.data.model.ApplicationUser;
+import com.molva.server.data.model.Profile;
+import com.molva.server.data.service.ApplicationUserService;
+import com.molva.server.data.service.ProfileService;
+import com.molva.server.helpers.ApplicationUserFactory;
+import com.molva.server.security.roles.ApplicationUserRole;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class RegistrationControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ApplicationUserFactory factory;
+
+  @MockBean
+  ApplicationUserService userService;
+
+  @MockBean
+  ProfileService profileService;
+
+
+  @Test
+  @WithMockUser(roles = "ADMIN")
+  void registrationModeratorMustReturnOkIfValidRequest() throws Exception {
+    ApplicationUser user = factory.createRegisteredModerator();
+    var jsonBody = new JSONObject(
+        Map.of(
+            "username", user.getUsername(),
+            "password", user.getPassword(),
+            "email", user.getEmail()));
+
+    doReturn(user).when(userService).registerUser(any(ApplicationUser.class), any(ApplicationUserRole.class));
+    doReturn(null).when(profileService).addProfile(any(Profile.class), any(ApplicationUser.class));
+    doReturn(user).when(userService).updateApplicationUserById(any(Long.class), any(ApplicationUser.class));
+
+    mockMvc.perform(post("/admin/api/v1/registration/register/moderator")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(jsonBody.toString())
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.username").value(user.getUsername()))
+        .andExpect(jsonPath("$.email").value(user.getEmail()));
+  }
+
+}
