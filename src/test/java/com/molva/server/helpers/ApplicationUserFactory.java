4fbf3ded9d3254e9dae0f33a85b45d34691b25c2
Georgii <49598870+corradowaver@users.noreply.github.com>
2020-08-11 19:58:10 +0300

    improvement

--- src/test/java/com/molva/server/helpers/ApplicationUserFactory.java
+++ src/test/java/com/molva/server/helpers/ApplicationUserFactory.java
@@ -1,7 +1,6 @@
 package com.molva.server.helpers;
 
 import com.molva.server.data.model.ApplicationUser;
-import com.molva.server.data.model.Profile;
 import com.molva.server.security.roles.ApplicationUserRole;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,8 +19,10 @@ public class ApplicationUserFactory {
     return List.of(createApplicationUser(), createApplicationUser(), createApplicationUser());
   }
 
-  public ApplicationUser createRegisteredModerator() {
-    return new ApplicationUser("username", "password", "email@mail.com",
+  public ApplicationUser createRegisteredModerator(long id) {
+    var moderator = new ApplicationUser("username", "password", "email@mail.com",
         ApplicationUserRole.MODERATOR, null, true, true, true, true);
+    moderator.setId(id);
+    return moderator;
   }
 }
