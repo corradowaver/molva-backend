ef074c4d8bb92ba475ea94025dbb9840f0cddc3e
Georgii <49598870+corradowaver@users.noreply.github.com>
2020-08-21 00:47:18 +0300

    better impl

--- src/main/java/com/molva/server/ServerApplication.java
+++ src/main/java/com/molva/server/ServerApplication.java
@@ -1,7 +1,10 @@
 package com.molva.server;
 
+import com.molva.server.data.model.ApplicationUser;
+import com.molva.server.data.model.Profile;
 import com.molva.server.data.service.ApplicationUserService;
 import com.molva.server.data.service.ProfileService;
+import com.molva.server.security.roles.ApplicationUserRole;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,17 +21,18 @@ public class ServerApplication {
     app.run(args);
   }
 
-    @Bean
-    public CommandLineRunner test(ApplicationUserService applicationUserService, ProfileService profileService) {
-      return args -> {
-//        applicationUserService.deleteApplicationUserById(23L);
-//        ApplicationUser ap = applicationUserService.registerUser(new ApplicationUser("admin", "123456l", "iamnzrv@gmail.com"), ApplicationUserRole.ADMIN);
-//        Profile profile = new Profile();
-//        profile.setApplicationUser(ap);
-//        Profile p = profileService.addProfile(profile, ap);
-//        ap.setProfile(p);
-//        applicationUserService.updateApplicationUserById(ap.getId(), ap);
-      };
-    }
+  @Bean
+  public CommandLineRunner test(ApplicationUserService applicationUserService, ProfileService profileService) {
+    return args -> {
+//      ApplicationUser admin = applicationUserService.registerUser(
+//          new ApplicationUser("admin", "123456l", "majorwombat@rainbow.com"),
+//          ApplicationUserRole.ADMIN);
+//      Profile profile = new Profile();
+//      profile.setApplicationUser(admin);
+//      Profile p = profileService.addProfile(profile, admin);
+//      admin.setProfile(p);
+//      applicationUserService.updateApplicationUserById(admin.getId(), admin);
+    };
+  }
 
 }
