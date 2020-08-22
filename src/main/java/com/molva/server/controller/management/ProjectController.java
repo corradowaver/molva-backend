96a86d675bd8f2a4345d05a28168064e316c1194
iamnzrv <nzrvofficial@gmail.com>
2020-08-22 15:24:03 +0300

    refactored project controller + service

--- src/main/java/com/molva/server/controller/management/ProjectController.java
+++ src/main/java/com/molva/server/controller/management/ProjectController.java
@@ -3,9 +3,7 @@ package com.molva.server.controller.management;
 import com.molva.server.data.exceptions.profile.ProfileExceptions;
 import com.molva.server.data.model.ApplicationUser;
 import com.molva.server.data.model.Project;
-import com.molva.server.data.model.MediaFile;
 import com.molva.server.data.service.ApplicationUserService;
-import com.molva.server.data.service.MediaFileService;
 import com.molva.server.data.service.ProjectService;
 import com.molva.server.data.service.helpers.FileValidators;
 import com.molva.server.data.service.helpers.ProjectValidators;
@@ -17,9 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("management/api/v1/projects")
@@ -27,19 +22,16 @@ public class ProjectController {
 
   private final ProjectService projectService;
   private final ApplicationUserService applicationUserService;
-  private final MediaFileService mediaFileService;
   private final JwtProvider jwtProvider;
 
   @Autowired
   public ProjectController(
       ProjectService projectService,
       ApplicationUserService applicationUserService,
-      MediaFileService mediaFileService,
       JwtProvider jwtProvider
   ) {
     this.projectService = projectService;
     this.applicationUserService = applicationUserService;
-    this.mediaFileService = mediaFileService;
     this.jwtProvider = jwtProvider;
   }
 
@@ -49,19 +41,7 @@ public class ProjectController {
                             Project project,
                             @RequestParam("preview-multipart") MultipartFile preview) {
     validateProjectData(project, preview, null);
-    ApplicationUser applicationUser = applicationUserService.loadAccountByUsername(
-        jwtProvider.getUsername(jwtProvider.resolveToken(token))
-    );
-    project.setApplicationUser(applicationUser);
-    Project savedProject = projectService.addProject(project);
-    applicationUser.addProject(savedProject);
-    applicationUserService.updateApplicationUserById(applicationUser.getId(), applicationUser);
-    MediaFile mediaFile = mediaFileService.addMediaFile(preview);
-    savedProject.setPreview(mediaFile);
-    projectService.updateProjectById(savedProject.getId(), savedProject);
-    mediaFile.setPreviewOwner(savedProject);
-    mediaFileService.updateMediaFileById(mediaFile.getId(), mediaFile);
-    return savedProject;
+    return projectService.addProject(project, token, preview);
   }
 
   @PutMapping(path = "/edit/{projectId}")
@@ -70,37 +50,17 @@ public class ProjectController {
                                @RequestHeader("Authorization") String token,
                                Project projectData,
                                @RequestParam("preview-multipart") MultipartFile preview,
-                               @RequestParam("files-multipart") MultipartFile[] files) {
+                               @RequestParam(value = "files-multipart", required = false) MultipartFile[] files) {
     throwAnExceptionIfProjectIdNotMatchesUserProjects(token, projectId);
     validateProjectData(projectData, preview, files);
-    Project project = projectService.loadProjectById(projectId);
-    project.setName(projectData.getName());
-    project.setDescription(projectData.getDescription());
-    MediaFile oldPreview = project.getPreview();
-    if (oldPreview != null) {
-      mediaFileService.deleteMediaFileById(oldPreview.getId());
-    }
-    Set<MediaFile> oldFiles = project.getFiles();
-    if (oldFiles != null) {
-      oldFiles.forEach(oldFile -> {
-        mediaFileService.deleteMediaFileById(oldFile.getId());
-      });
-    }
-    MediaFile newPreview = mediaFileService.addMediaFile(preview);
-    project.setPreview(newPreview);
-    Set<MediaFile> newFiles = Arrays.stream(files)
-        .filter(Objects::nonNull)
-        .map(mediaFileService::addMediaFile)
-        .collect(Collectors.toSet());
-    project.setFiles(newFiles);
-    projectService.updateProjectById(projectId, project);
-    newPreview.setPreviewOwner(project);
-    mediaFileService.updateMediaFileById(newPreview.getId(), newPreview);
-    newFiles.forEach(file -> {
-      file.setFileOwner(project);
-      mediaFileService.updateMediaFileById(file.getId(), file);
-    });
-    return project;
+    return projectService.updateProjectById(projectId, projectData, preview, files);
+  }
+
+  @DeleteMapping(path = "/delete/{projectId}")
+  public void deleteProject(@RequestHeader("Authorization") String token,
+                            @NotNull @PathVariable("projectId") Long projectId) {
+    throwAnExceptionIfProjectIdNotMatchesUserProjects(token, projectId);
+    projectService.deleteProjectById(projectId);
   }
 
   public void validateProjectData(
@@ -114,22 +74,6 @@ public class ProjectController {
     if (files != null) {
       Arrays.stream(files).forEach(FileValidators::validateMediaFile);
     }
-  }
-
-  @DeleteMapping(path = "/delete/{projectId}")
-  public void deleteProject(@RequestHeader("Authorization") String token,
-                            @NotNull @PathVariable("projectId") Long projectId) {
-    throwAnExceptionIfProjectIdNotMatchesUserProjects(token, projectId);
-    Project project = projectService.loadProjectById(projectId);
-    if (project.getPreview() != null) {
-      mediaFileService.deleteMediaFileById(project.getPreview().getId());
-    }
-    if (project.getFiles() != null) {
-      project.getFiles().forEach(file -> {
-        mediaFileService.deleteMediaFileById(file.getId());
-      });
-    }
-    projectService.deleteProjectById(projectId);
   }
 
   private void throwAnExceptionIfProjectIdNotMatchesUserProjects(String token, Long id) {
--- src/main/java/com/molva/server/data/service/ProjectService.java
+++ src/main/java/com/molva/server/data/service/ProjectService.java
@@ -1,25 +1,36 @@
 package com.molva.server.data.service;
 
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobId;
 import com.molva.server.data.exceptions.project.ProjectExceptions;
 import com.molva.server.data.model.ApplicationUser;
+import com.molva.server.data.model.MediaFile;
 import com.molva.server.data.model.Project;
 import com.molva.server.data.repository.ProjectRepository;
+import com.molva.server.security.jwt.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
   private final ProjectRepository projectRepository;
+  private final MediaFileService mediaFileService;
+  private final ApplicationUserService applicationUserService;
+  private final JwtProvider jwtProvider;
 
   @Autowired
-  ProjectService(ProjectRepository projectRepository) throws IOException {
+  ProjectService(
+      ProjectRepository projectRepository,
+      MediaFileService mediaFileService,
+      ApplicationUserService applicationUserService,
+      JwtProvider jwtProvider
+  ) {
     this.projectRepository = projectRepository;
+    this.mediaFileService = mediaFileService;
+    this.applicationUserService = applicationUserService;
+    this.jwtProvider = jwtProvider;
   }
 
   public List<Project> loadAllProjects() {
@@ -38,7 +49,61 @@ public class ProjectService {
         .orElseThrow(ProjectExceptions.ProjectNotFoundException::new);
   }
 
-  public Project addProject(Project project) {
+  public Project addProject(Project project, String token, MultipartFile preview) {
+    ApplicationUser applicationUser = applicationUserService.loadAccountByUsername(
+        jwtProvider.getUsername(jwtProvider.resolveToken(token))
+    );
+    project.setApplicationUser(applicationUser);
+    Project savedProject = saveProjectIfThereIsNoneWithTheSameName(project);
+    addProjectToUserProjects(applicationUser, savedProject);
+    savedProject.setPreview(saveSingleFileToStorage(preview));
+    updateProjectById(savedProject.getId(), savedProject, preview, null);
+    setFileOwnerProjectForSingleFile(savedProject.getPreview(), savedProject);
+    return projectRepository.save(project);
+  }
+
+  public Project updateProjectById(
+      Long id,
+      Project newProject,
+      MultipartFile preview,
+      MultipartFile[] files
+  ) {
+    Project project = loadProjectById(id);
+    project.setName(newProject.getName());
+    project.setDescription(newProject.getDescription());
+    deleteOldFileIfItExists(project.getPreview());
+    project.setPreview(saveSingleFileToStorage(preview));
+    if (files != null) {
+      project.getFiles().forEach(this::deleteOldFileIfItExists);
+      project.setFiles(saveAllFilesToStorage(files));
+    }
+    project.getPreview().setPreviewOwner(project);
+    setFileOwnerProjectForSingleFile(project.getPreview(), project);
+    setFileOwnerProjectForEachFile(project.getFiles(), project);
+    return projectRepository.save(project);
+  }
+
+  public void deleteProjectById(Long id) {
+    Project project = loadProjectById(id);
+    deleteOldFileIfItExists(project.getPreview());
+    if (project.getFiles() != null) {
+      project.getFiles().forEach(this::deleteOldFileIfItExists);
+    }
+    projectRepository.deleteById(id);
+  }
+
+  public void deleteOldFileIfItExists(MediaFile file) {
+    if (file != null) {
+      mediaFileService.deleteMediaFileById(file.getId());
+    }
+  }
+
+  private void addProjectToUserProjects(ApplicationUser applicationUser, Project project) {
+    applicationUser.addProject(project);
+    applicationUserService.updateApplicationUserById(applicationUser.getId(), applicationUser);
+  }
+
+  private Project saveProjectIfThereIsNoneWithTheSameName(Project project) {
     Optional<Project> projectWithTheSameName =
         projectRepository.findProjectByName(project.getName());
     if (projectWithTheSameName.isPresent()) {
@@ -47,22 +112,26 @@ public class ProjectService {
     return projectRepository.save(project);
   }
 
-  public Project updateProjectById(Long id, Project newProject) {
-    Optional<Project> projectOptional = projectRepository.findById(id);
-    if (projectOptional.isPresent()) {
-      newProject.setId(projectOptional.get().getId());
-      return projectRepository.save(newProject);
-    } else {
-      throw new ProjectExceptions.ProjectNotFoundException();
-    }
+  private Set<MediaFile> saveAllFilesToStorage(MultipartFile[] files) {
+    return Arrays.stream(files)
+        .filter(Objects::nonNull)
+        .map(mediaFileService::addMediaFile)
+        .collect(Collectors.toSet());
   }
 
-  public void deleteProjectById(Long id) {
-    Optional<Project> projectOptional = projectRepository.findById(id);
-    if (projectOptional.isPresent()) {
-      projectRepository.deleteById(id);
-    } else {
-      throw new ProjectExceptions.ProjectNotFoundException();
-    }
+  private void setFileOwnerProjectForEachFile(Set<MediaFile> mediaFiles, Project owner) {
+    mediaFiles.forEach(file -> {
+      file.setFileOwner(owner);
+      mediaFileService.updateMediaFileById(file.getId(), file);
+    });
+  }
+
+  private void setFileOwnerProjectForSingleFile(MediaFile mediaFile, Project owner) {
+    mediaFile.setPreviewOwner(owner);
+    mediaFileService.updateMediaFileById(owner.getId(), owner.getPreview());
+  }
+
+  private MediaFile saveSingleFileToStorage(MultipartFile file) {
+    return mediaFileService.addMediaFile(file);
   }
 }
